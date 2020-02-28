3991984901c46517e97cb3463a28d0ac6d4d975c
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-28 16:17:15 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,6 +8,19 @@ class Patient
   end 
   
   def new_appointment(doctor, date)
-    Appointment.new(self, date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def appointments 
+    Appointment.all.select{|appt| appt.patient == self} 
+  end 
+  
+  def doctors 
+    appointments.map {|appt| appt.doctor}
+  end 
   
 end 
