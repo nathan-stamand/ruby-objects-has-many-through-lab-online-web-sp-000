b1a58de844c975396e6899e2069fc33d6c6418d0
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-28 16:18:13 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,21 @@
+class Genre 
+  attr_accessor :name
+  @@all = [] 
+  
+  def initialize(name)
+    @name=name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs 
+    Song.all.select {|song| song.genre == self}
+  end 
+  
+  def artists 
+    Artist.all.select {|artist| artist.genres.select {|genre| genre == self}}
+  end
+end 
